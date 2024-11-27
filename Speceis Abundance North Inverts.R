4262d75f7401d49d3362536fedb42a38ba1d6305
Matty-Plumtree <s2704659@ed.ac.uk>
2024-11-27 20:05:11 +0000

    This is the first commit for order abundance on the North site

--- Speceis Abundance North Inverts.R
+++ Speceis Abundance North Inverts.R
@@ -2,13 +2,13 @@ library(readxl)
 library(ggplot2)
 
 # This reads in our excel spreadsheet from arran
-NorthInvert <- read_excel("ArranExcel.xlsx",sheet="South side invert transects")
+NorthInvert <- read_excel("ArranExcel.xlsx",sheet="North side invert transects")
 
 
 # I had to get help from stack overflow for reording order and individual counts
-ggplot(SouthInvert, aes(x = reorder(order, -individualCount), y = as.numeric(individualCount), fill = order)) +
+ggplot(NorthInvert, aes(x = reorder(order, -individualCount), y = as.numeric(individualCount), fill = order)) +
   geom_bar(stat = "identity", colour = 'black') + # ensures that these values are used directly for the bar heights, instead of counting the occurrences of order.
-  labs(title = "Order Abundance in South Side Transects",
+  labs(title = "Order Abundance in North Side Transects",
        x = "Invertebrate Order",
        y = "Individual Counts",
        fill = "Order"
